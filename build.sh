671fd7ec96c96265cc7dac20fe709ad1d98de8a8
imanesaurus <pirmansah.otoluwa@gmail.com>
2014-01-05 08:05:27 +0800

    updated build.sh also deleted unsupported languange in framework

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$1 $2
+$1 $2 $3
 cd ..
 . build/envsetup.sh
 cd huashan
@@ -9,4 +9,7 @@ make fullota PORT_PRODUCT=$1 BUILD_NUMBER=$2
 
 rmdir AntiSpam Backup Browser BugReport Calculator Calendar CalendarProvider CloudService Contacts ContactsProvider DeskClock DownloadProvider DownloadProviderUi Email Exchange2 FileExplorer MiuiCompass MiuiGallery MiuiVideoPlayer MiWallpaper Music SettingsProvider MusicFX LBESEC_MIUI GuardProvider NetworkAssistant Notes PackageInstaller PaymentService Provision QuickSearchBox SoundRecorder TelephonyProvider Transfer VpnDialogs Weather WeatherProvider XiaomiServiceFramework YellowPage
 
+cp -rf out/MiuiSystemUI backupfiles/MiuiSystemUI$2
+
+./copydisk.sh $1 $2 $3
 exit
